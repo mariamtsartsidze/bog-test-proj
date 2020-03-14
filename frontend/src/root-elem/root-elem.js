0e2273fe07f67b4d8b48fae54260c18f7917b55e
mariamtsartsidze <mtsar16@freeuni.edu.ge>
2020-03-14 22:28:39 +0400

    refactor: cleaner way, lit-element component and no unnecessary margins

--- frontend/src/root-elem/root-elem.js
+++ frontend/src/root-elem/root-elem.js
@@ -1,77 +1,93 @@
-import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
-
+import { LitElement, css, html } from 'lit-element';
 /**
  * @customElement
  * @polymer
  */
-class RootElem extends PolymerElement {
+class RootElem extends LitElement {
+  static get properties() {
+    return {
+      transactions: { type: Array },
+    };
+  }
+
   constructor() {
     super();
+
+    this._transactions = [];
+
     var req = new XMLHttpRequest();
     req.responseType = 'json';
     req.open('GET', 'http://localhost:3000/transactions', true);
-    req.onload = function () {
-      var jsonResponse = req.response;
-      console.log('resp: ', jsonResponse);
+    req.onload = () => {
+      this.transactions = req.response.transactions;
+      console.log('transactions: ', this.transactions);
     };
     req.send(null);
   }
 
-  static get template() {
+  set transactions(val) {
+    console.log('gonan set val: ', val);
+    let oldVal = this._transactions;
+    this._transactions = [...val];
+    this.requestUpdate('transactions', oldVal);
+  }
+
+  get transactions() {
+    console.log('im getting: ', this._transactions);
+    return this._transactions;
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+      }
+      .header {
+        background-color: #f5fafa;
+        height: 6vh;
+        font-size: 13px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        color: grey;
+      }
+      .footer {
+        background-color: #f5fafa;
+        height: 9vh;
+        text-align: center;
+        font-size: 14px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        color: grey;
+      }
+      .section {
+        width: 100vw;
+        height: 85vh;
+        background-color: #d2ddeb;
+      }
+    `;
+  }
+
+  render() {
     return html`
-      <style>
-        :host {
-          display: block;
-        }
-        header {
-          background-color: #f5fafa;
-          height: 6vh;
-          font-size: 13px;
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-          justify-content: center;
-          color: grey;
-        }
-        footer {
-          background-color: #f5fafa;
-          height: 9vh;
-          text-align: center;
-          font-size: 14px;
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-          justify-content: center;
-          color: grey;
-        }
-        section {
-          width: 100vw;
-          height: 83vh;
-          background-color: #bdd8f2;
-        }
-      </style>
-      <header>
+      <div class="header">
         <div>hello user</div>
-      </header>
-      <section>
-        <div>Main Content</div>
-      </section>
-      <footer>
+      </div>
+      <div class="section">
+        ${this.transactions.map(i => html`<div>${i}</div>`)}
+      </div>
+      <div class="footer">
         <div>
           <div>2020</div>
           <div>payment menagement</div>
         </div>
-      </footer>
+      </div>
     `;
   }
-  static get properties() {
-    return {
-      prop1: {
-        type: String,
-        value: 'root-elem'
-      }
-    };
-  }
+
 }
 
 window.customElements.define('root-elem', RootElem);
